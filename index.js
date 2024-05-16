63c3d95e9e2fa29dad543f7c37830c07cfa223f3
steveh2707 <89089686+steveh2707@users.noreply.github.com>
2024-05-16 16:38:01 +0100

    UI improved (#5)

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const DIRECTORY_PATH = './data';
 const app = express();
 
-const db = new Database('appdata/app.db')
+let db = new Database('appdata/app.db')
 const bookRepository = new BookRepository(db);
 const bookService = new BookService(bookRepository, DIRECTORY_PATH);
 
@@ -29,7 +29,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/data', express.static(path.join(__dirname, 'data')));
 
 app.get('/', (req, res) => {
-    res.render('index', {audiobooks: bookService.getAllBooks()})
+    res.render('discover', {audiobooks: bookService.getAllBooks()})
+})
+
+app.post('/read/:id', (req,res) => {
+    const id = req.params.id
+    bookService.markBookAsRead(id)
+    res.redirect('/')
+})
+
+app.post('/unread/:id', (req,res) => {
+    const id = req.params.id
+    bookService.markBookAsUnread(id)
+    res.redirect('/')
 })
 
 app.get('/download', (req, res) => {
